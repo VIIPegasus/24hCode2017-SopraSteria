57f433d95a6b641ef5e529c099c01dc99e3a4d2c
Videl <videl@fslhome.org>
2017-01-22 07:39:34 +0100

    Revert "A"
    This reverts commit 8d633fa0b8476604c1b52e6ea1a752f9b7d210f3.

--- src/public/app/main.js
+++ src/public/app/main.js
@@ -1,16 +1,12 @@
 "use strict";
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-var app_module_1 = require("./components/app/app.module");
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
-/*
-
-
-if(_root.globals.enviroment != "development"){
-  enableProdMode();
+var app_components_1 = require("./components/app/app.components");
+var app_routes_1 = require("./components/routes/app.routes");
+var core_1 = require("@angular/core");
+if (_root.globals.enviroment != "development") {
+    core_1.enableProdMode();
 }
-
-bootstrap(AppModule, [
-  APP_ROUTER_PROVIDERS
+platform_browser_dynamic_1.bootstrap(app_components_1.AppComponent, [
+    app_routes_1.APP_ROUTER_PROVIDERS
 ]);
-*/
 //# sourceMappingURL=main.js.map
--- src/public/systemjs.config.js
+++ src/public/systemjs.config.js
@@ -8,19 +8,10 @@
   var map = {
     'app':                        'app', // 'dist',
 
-    '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-    '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-    '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-    '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-    '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-    '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-    '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-    '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-
-    // other libraries
-    'rxjs':                      'npm:rxjs',
-    'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
-
+    '@angular':                   'node_modules/@angular',
+    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+    'rxjs':                       'node_modules/rxjs',
+    'ang2-phaser':                'node_modules/ang2-phaser'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
@@ -64,9 +55,6 @@
   packages['@angular/router'] = { main: 'index.js', defaultExtension: 'js' };
 
   var config = {
-    paths: {
-        'npm:': 'node_modules/'
-    },
     map: map,
     packages: packages
   };
